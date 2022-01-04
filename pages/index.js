ba099c5db5aa125a8b288a916d44c998044e769c
Ben Toms <me@ben-toms.com>
2022-01-04 15:27:19 +0000

    added big text block

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
       </Head>
 
       <div>
-        <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div className="px-4 sm:py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
           <div className="max-w-xl sm:mx-auto lg:max-w-2xl">
             <div className="flex flex-col mb-16 sm:text-center sm:mb-0">
               <div className="flex items-center justify-center w-30 h-30 rounded-full mb-6">
@@ -56,6 +56,9 @@ export default function Home() {
             </div>
           </div>
         </div>
+      </div>
+      <div class="big-text text-center sm:my-16 my-5">
+        <h2 className="font-black text-9xl bg-gray-100 mix-blend-lighten uppercase">for the <br/> love of code</h2>
       </div>
       <section className="bg-gray-800 pattern py-20">
         <div className="max-w-5xl px-6 mx-auto text-center">
