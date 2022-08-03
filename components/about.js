609035df2dc59773edb69e98d941b4f6deac48f3
Ben <bentoms30@gmail.com>
2022-08-03 17:08:37 +0200

    Rebrand

---
+++ components/about.js
@@ -0,0 +1,76 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import me from '.././public/ben-toms.jpg'
+import styles from '../styles/Home.module.css'
+
+export default function About() {
+    return (
+    <div className='container'>
+        <div className={styles.container}>
+            <div className='grid grid-cols-1 lg:grid-cols-3 justify-items-end mb-10 lg:mb-28'>
+                <div className='grid grid-cols-1 md:grid-cols-3 col-span-2'>
+                    <div className='grid grid-cols-1'>
+                        <Image
+                        className="lg:w-screen lg:h-screen object-fit object-contain lg:object-cover"
+                        src={me} alt="A picture of Ben Toms"/>
+                        <span>
+                            BEN TOMS
+                        </span>
+                        <Link
+                            passHref
+                            href="mailto:me@ben-toms.com"
+                            target="_blank"
+                        >
+                            Ready to talk? Let&apos;s go →
+                        </Link>
+                    </div>
+                    <div className='grid lg:pr-28 lg:pl-4 lg:col-span-2'>
+                        <h1 className='text__pull-up'>DEVELOPER</h1>
+                        <p><span className="text__callout">I&apos;VE BEEN DOING THIS A LONG TIME.</span> With 14+ years experience, I have seen it all. I&apos;ve been through site migrations, replatforms, 5am launches, 2am emergency calls, ridiculous bugs and much more.</p>
+                        <p>I have over 6 years experience developing in E-Commerce environments for international brands such as Dr. Martens, Missoma and Samsung. I&apos;ve also spent a few years freelancing around London agencies, working on multiple projects for global companies such as Samsung, ExxonMobil and Spotify which lead to extensive knowledge in both B2B and B2C as well as developing excellent client facing skills.</p>
+                        <p>My love for the Front-End gives me proficiencies in HTML, CSS, VanillaJS, PHP, MySQL and I&apos;m also highly experienced in NodeJS, NextJS, ReactJs and React Native as well as the popular CMS foundations WordPress, Shopify and Hybris.</p>
+                        <p>I am also highly experienced in building Headless CMS solutions and using cloud platforms such as AWS, Netlify and Heroku.</p>
+                    </div>
+                </div>
+                <div className='lg:text-right grid lg:pl-17'>
+                    <h2 className='text__push-down'>LEADER</h2>
+                    <p><span className="text__callout">BUILDING THE DREAM TEAM.</span> I&apos;ve had many opportunities in my career to be in the position of building and leading development teams. From a small 3 piece to a strong 6 I&apos;ve been able to mentor, shape and also learn from other developers. I have a passion for training others and also learning from them. I&apos;m never going to pretend to know everything because that&apos;s never possible. </p>
+                </div>
+            </div>
+            <div className='grid grid-cols-1 justify-items-center text-center mb-10 lg:mb-28'>
+                <span className="w-full p-0-2 bg-white lg:w-1/3 mb-10"></span>
+                <p>
+                <span className="text__callout">I&apos;VE ALWAYS HAS A PASSING FOR BUILDING APPLICATIONS.</span> Creating features and functions that people can interact with is really excfiting for me.<br/>
+                    This is why I love working in the B2C sector as I get to see how people interact with the products I put out and I learn and improve from the feedback.
+                </p>
+                <span className="w-full p-0-2 bg-white lg:w-1/3 mt-10"></span>
+            </div>
+            <div className='grid grid-cols-1 text-left'>
+                <h2 className='text__pull-up'>WHAT I LIKE <span className='hidden lg:inline-block'>WORKING WITH</span></h2>
+                <ul className='list-none columns-3 w-25'>
+                    <li>javascript</li>
+                    <li>react</li>
+                    <li>nextjs</li>
+                    <li>git</li>
+                    <li>html</li>
+                    <li>scss</li>
+                    <li>kali</li>
+                    <li>ci/cd</li>
+                    <li>coffee</li>
+                </ul>
+            </div>
+            <div className='grid grid-cols-1 text-left lg:text-right lg:justify-items-end'>
+                <h2 className='text__pull-up hidden lg:block'>WHAT I LIKE WORKING ON</h2>
+                <ul className='list-none columns-3 lg:columns-2 w-25'>
+                    <li>e-commerce</li>
+                    <li>apps</li>
+                    <li>dashboads</li>
+                    <li>perf</li>
+                    <li>pen test</li>
+                    <li>challenges</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    )
+}
--- pages/about.js
+++ pages/about.js
@@ -1,95 +1,15 @@
 import Header from '../components/header'
-import Sidebar from '../components/sidebar'
+import Topbar from '../components/topBar'
 import Footer from '../components/footer'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import picture from '.././public/ben-toms-about-2.jpg';
-import picture2 from '.././public/ben-toms-about-3.jpg';
+
 
 export default function About() {
   return (
-    <div>
-        <Header/>
-        <Sidebar/>
-        <div className={styles.container}>
-
-            <div className="container mx-auto">
-                <div className="grid grid-cols-1 md:grid-cols-2">
-                    <div className="flex justfiy-center">
-                        <Image
-                        className="w-screen h-screen object-cover object-top"
-                        src={ picture }
-                        alt="Ben Toms Portrait"
-                        />
-                    </div>
-                    <div className="flex text-gray-800 justify-center">
-                        <div className="mb-auto mt-auto max-w-xlg bg-white p-10">
-                        <h1 className="max-w-lg mb-1 font-sans text-3xl font-bold leading-none tracking-tight text-gray-800 sm:text-4xl">
-                            Ben Toms
-                        </h1>
-                        <p className="font-semibold mb-5">Fullstack Developer</p>
-                        <p className="mb-5">
-                            With 14+ years experience, I understand what makes a site flow and the importance of strong code.
-                            My design, music and art background allows me to bring a uniquely influenced style to my work that most developers don&apos;t have.
-                        </p>
-                        <p className="mb-5">
-                            I have over 6 years experience developing in E-Commerce environments for international brands such as Dr. Martens, Missoma and CaféDirect.
-                        </p>
-                        <p className="mb-5">
-                            I&apos;ve also spent a few years freelancing around London agencies, working on multiple projects for global companies such as Samsung, ExxonMobil and Spotify which lead to extensive knowledge in both B2B and B2C as well as developing excellent client facing skills.
-                        </p>
-                        <p className="mb-5">
-                            My love for the Front-End gives me proficiencies in HTML, CSS, VanillaJS, PHP, MySQL and I&apos;m also highly experienced in NodeJS, NextJS, ReactJs and React Native as well as the popular CMS foundations WordPress, Shopify, Umbraco and Hybris.
-                        </p>
-                        <p className="mb-5">
-                            I am also highly experienced in building Headless CMS solutions and using cloud platforms such as AWS, Netlify and Heroku.
-                        </p>
-                        <a href="mailto:me@ben-toms.com"><button className="bg-neutral-900 hover:bg-neutral-400 rounded-md py-3 px-7 mt-6 text-white">Let&apos;s go</button></a>
-                        </div>
-                    </div>
-                </div>
-                <div className='grid grid-cols-1 mt-10 mb-10'>
-                    <div className="text-white bg-neutral-900 px-5 py-5 md:p-10 text-center">
-                        <h2 className="font-serif text-2xl md:text-6xl mb-10">I&apos;ve always had a passion for building applications</h2>
-                        <p>Creating features and functions that people can interact with is really exicting for me.</p>
-                        <p>This is why I love working in the B2C sector as I get to see how people interact with the products I put out and I learn and improve from the feedback.</p>
-                    </div>
-                </div>
-                <div className="grid grid-cols-1 md:grid-cols-2">
-
-                    <div className="flex text-gray-800 justify-center">
-                        <div className="w-screen bg-white p-10">
-                            <h2 className="font-semibold text-xl">What I like working with</h2>
-                            <ul className='mb-5'>
-                                <li>Javascript (ES6+)</li>
-                                <li>React</li>
-                                <li>Next</li>
-                                <li>SCSS</li>
-                                <li>HTML</li>
-                                <li>Git</li>
-                            </ul>
-                            <h2 className="font-semibold text-xl">What I like working on</h2>
-                            <ul>
-                                <li>E-commerce sites</li>
-                                <li>Apps</li>
-                                <li>User dashboards</li>
-                                <li>Performance improvements</li>
-                                <li>Penetration testing</li>
-                                <li>Fun projects</li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="flex justfiy-center">
-                        <Image
-                        className="w-screen h-screen object-cover object-top"
-                        src={ picture2 }
-                        alt="Ben Toms landscape"
-                        />
-                    </div>
-                 </div>
-            </div>
-            <Footer/>
-        </div>
-    </div>
+    <>
+    <Header/>
+    <Topbar />
+    <div>Errr...this isn&apos;t done. Sorry.</div>
+    <Footer/>
+    </>
    )
 }
