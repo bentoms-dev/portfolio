d117a365fa79f10b47fe5ded71d1c94cdcc0e08e
Ben <bentoms30@gmail.com>
2023-12-18 17:32:02 +0100

    added GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
 import '../styles/globals.css'
+import ReactGA from 'react-ga';
+  const TRACKING_ID = "G-86E829YV2S"; // OUR_TRACKING_ID
+  ReactGA.initialize(TRACKING_ID);
 
 function MyApp({ Component, pageProps }) {
-
-
-
   return <Component {...pageProps} />
 }
 
